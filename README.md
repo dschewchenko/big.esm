8e9c2fccf23887ca2699d6120dce42733e25945d
Dmytro Shevchenko <ds@nemesiscorporation.com>
2023-05-13 23:02:20 +0200

    fix: update readme and add benchmark

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Library for working with large numbers and fractions using `BigInt`.
 It provides advanced functionality for performing arithmetic operations with precision and decimal handling.
+[Documentation](https://dschewchenko.github.io/big.esm/).
 
 100% test coverage.
 
@@ -63,26 +64,15 @@ Creates a new `Big` instance from a string, number or `BigInt`. Optionally, you 
 Alias for `new Big(value)`.
 But it does not support the scale parameter and if the value is instance of `Big` it will be returned without changes.
 
-```js
-const a = createBig("12345678910.12345678910"); // value = 1234567891012345678910 ,scale = 11
-const b = createBig(12345678910.12345678910); // value = 1234567891012345678910 ,scale = 11
-const c = createBig(1234567891012345678910n); // value = 1234567891012345678910 ,scale = 0
-```
-
-### `cloneBig(a: Big): Big`
+#### `cloneBig(a: Big): Big`
 
 Creates a new `Big` instance from another `Big` instance.
 
-```js
-const a = createBig("12345678910.12345678910");
-const b = cloneBig(a);
-```
-
-### `alignScale(a: Big, b: Big): [Big, Big]`
+#### `alignScale(a: Big, b: Big): [Big, Big]`
 
 Aligns the scale of two `Big` instances. The scale of the result will be equal to the maximum scale of the two numbers.
 
-### `isNumericValue(value: any): boolean`
+#### `isNumericValue(value: any): boolean`
 
 Checks if the value is a valid numeric value for creating a `Big` instance.
 
@@ -119,6 +109,58 @@ Calculates the square root of `a`.
 #### `absBig(a: Big): Big`
 
 Returns the absolute value of `a`.
+
+#### `compareBig(a: Big, b: Big): -1 | 0 | 1`
+
+Compares two `Big` instances. Returns `-1` if `a < b`, `0` if `a == b` and `1` if `a > b`.
+
+## Benchmark
+
+Compare `big.esm` with `big.js`. 100_000 iterations for each operation.
+
+| Operation | big.esm | big.js | Difference |
+| --- | --- | --- | --- |
+| init - smallInt | 40.538ms | 23.042ms | -75.93% slower |
+| add - smallInt | 25.755ms | 15.443ms | -66.77% slower |
+| subtract - smallInt | 22.998ms | 15.419ms | -49.15% slower |
+| multiply - smallInt | 22.684ms | 19.931ms | -13.81% slower |
+| divide - smallInt | 67.082ms | 182.769ms | 63.30% faster |
+| power of 4 - smallInt | 115.589ms | 40.343ms | -186.51% slower |
+| init - smallNumber | 45.344ms | 37.790ms | -19.99% slower |
+| add - smallNumber | 21.636ms | 22.357ms | 3.23% faster |
+| subtract - smallNumber | 21.750ms | 24.691ms | 11.91% faster |
+| multiply - smallNumber | 23.852ms | 35.742ms | 33.27% faster |
+| divide - smallNumber | 61.577ms | 194.170ms | 68.29% faster |
+| power of 4 - smallNumber | 137.407ms | 70.480ms | -94.96% slower |
+| init - bigInt | 51.163ms | 37.148ms | -37.73% slower |
+| add - bigInt | 27.229ms | 25.447ms | -7.00% slower |
+| subtract - bigInt | 27.146ms | 24.872ms | -9.14% slower |
+| multiply - bigInt | 33.915ms | 124.638ms | 72.79% faster |
+| divide - bigInt | 81.944ms | 624.192ms | 86.87% faster |
+| power of 4 - bigInt | 251.438ms | 512.083ms | 50.90% faster |
+| init - bigNumber | 57.123ms | 77.780ms | 26.56% faster |
+| add - bigNumber | 30.457ms | 49.525ms | 38.50% faster |
+| subtract - bigNumber | 30.979ms | 49.906ms | 37.93% faster |
+| multiply - bigNumber | 41.303ms | 271.533ms | 84.79% faster |
+| divide - bigNumber | 81.252ms | 940.948ms | 91.36% faster |
+| power of 4 - bigNumber | 375.813ms | 1281.233ms | 70.67% faster |
+
+#### Test data:
+
+```js
+const smallIntA = 123;
+const smallIntB = 456;
+const smallNumberA = 123.456;
+const smallNumberB = 789.012;
+const bigIntA = "12345678901234567890";
+const bigIntB = "67890123456789012345";
+const bigNumberA = "12345678901234567890.1234567890";
+const bigNumberB = "67890123456789012345.6789012345";
+```
+
+Benchmarked on MacBook Pro M1 2021.
+
+Code for benchmarking is in the `benchmark.js` file. You can run it with `npm run benchmark`.
 
 ## Roadmap
 
---
+++ benchmark.js
@@ -0,0 +1,161 @@
+import { Big as BigJS } from "big.js";
+import { addBig, Big as BigESM, divBig, mulBig, powBig, subBig } from "./dist/big.es.js";
+
+import { performance } from "node:perf_hooks";
+
+const smallIntA = 123;
+const smallIntB = 456;
+const smallNumberA = 123.456;
+const smallNumberB = 789.012;
+const bigIntA = "12345678901234567890";
+const bigIntB = "67890123456789012345";
+const bigNumberA = "12345678901234567890.1234567890";
+const bigNumberB = "67890123456789012345.6789012345";
+
+// simple benchmark function
+function benchmark(name, func, times = 100_000) {
+  const start = performance.now();
+
+  for (let i = 0; i < times; i++) {
+    func();
+  }
+  const end = performance.now();
+
+  const elapsed = end - start;
+  console.log(`${name}: ${elapsed.toFixed(3)}ms`);
+  return elapsed;
+}
+
+console.log("Benchmarking with 100,000 iterations");
+
+let results = [];
+
+// run benchmark for each test case and operation
+function runBenchmark(testCases, operations) {
+  testCases.forEach((testCase) => {
+    operations.forEach((operation) => {
+      const { operationName, funcESM, funcJS } = operation;
+      const timeESM = benchmark(`big.esm — ${testCase.name} — ${operationName}`, funcESM(...testCase.args));
+      const timeJS = benchmark(`big.js — ${testCase.name} — ${operationName}`, funcJS(...testCase.args));
+
+      const diff = (timeESM - timeJS) / -timeJS * 100;
+      const diffFormatted = diff.toFixed(2);
+
+      const performanceResult = timeESM < timeJS ? "faster" : "slower";
+
+      results.push({
+        Operation: `${operationName} - ${testCase.name}`,
+        "big.esm": timeESM.toFixed(3),
+        "big.js": timeJS.toFixed(3),
+        Difference: `${diffFormatted}% ${performanceResult}`
+      });
+    });
+
+  });
+}
+
+// test cases (a, b)
+const testCases = [
+  {
+    name: "smallInt",
+    args: [smallIntA, smallIntB]
+  },
+  {
+    name: "smallNumber",
+    args: [smallNumberA, smallNumberB]
+  },
+  {
+    name: "bigInt",
+    args: [bigIntA, bigIntB]
+  },
+  {
+    name: "bigNumber",
+    args: [bigNumberA, bigNumberB]
+  }
+];
+
+// operations
+const operations = [
+  {
+    operationName: "init",
+    funcESM: (a, b) => () => {
+      const bigA = new BigESM(a);
+      const bigB = new BigESM(b);
+    },
+    funcJS: (a, b) => () => {
+      const bigA = new BigJS(a);
+      const bigB = new BigJS(b);
+    }
+  },
+  {
+    operationName: "add",
+    funcESM: (a, b) => {
+      const bigA = new BigESM(a);
+      const bigB = new BigESM(b);
+      return () => addBig(bigA, bigB);
+    },
+    funcJS: (a, b) => {
+      const bigA = new BigJS(a);
+      return () => bigA.plus(b);
+    }
+  },
+  {
+    operationName: "subtract",
+    funcESM: (a, b) => {
+      const bigA = new BigESM(a);
+      const bigB = new BigESM(b);
+      return () => subBig(bigA, bigB);
+    },
+    funcJS: (a, b) => {
+      const bigA = new BigJS(a);
+      return () => bigA.minus(b);
+    }
+  },
+  {
+    operationName: "multiply",
+    funcESM: (a, b) => {
+      const bigA = new BigESM(a);
+      const bigB = new BigESM(b);
+      return () => mulBig(bigA, bigB);
+    },
+    funcJS: (a, b) => {
+      const bigA = new BigJS(a);
+      return () => bigA.times(b);
+    }
+  },
+  {
+    operationName: "divide",
+    funcESM: (a, b) => {
+      const bigA = new BigESM(a);
+      const bigB = new BigESM(b);
+      return () => divBig(bigA, bigB);
+    },
+    funcJS: (a, b) => {
+      const bigA = new BigJS(a);
+      return () => bigA.div(b);
+    }
+  },
+  {
+    operationName: "power of 4",
+    funcESM: (a, b) => {
+      const bigA = new BigESM(a);
+      const bigB = new BigESM(b);
+      return () => powBig(bigA, 4);
+    },
+    funcJS: (a, b) => {
+      const bigA = new BigJS(a);
+      return () => bigA.pow(4);
+    }
+  }
+];
+
+runBenchmark(testCases, operations);
+
+// Markdown table generation
+let markdownTable = "| Operation | big.esm | big.js | Difference |\n";
+markdownTable += "| --- | --- | --- | --- |\n";
+for (const result of results) {
+  markdownTable += `| ${result.Operation} | ${result["big.esm"]}ms | ${result["big.js"]}ms | ${result.Difference} |\n`;
+}
+
+console.log(markdownTable);
