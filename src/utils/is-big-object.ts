3eb674ed53345537db203136ebb5879f8235d37b
Dmytro Shevchenko <ds@nemesiscorporation.com>
2023-05-19 12:40:00 +0200

    feat: add new utility functions: from-string, is-big-value

---
+++ src/utils/is-big-object.ts
@@ -0,0 +1,35 @@
+import type { BigObject } from "../types.ts";
+import type { Big } from "../big.ts";
+
+/**
+ * Determines if the value is a BigObject. A BigObject is an object with a value property of type bigint and a scale property of type number. It usually represents a Big instance.
+ *
+ * @param {Big | BigObject} value - The value to check. Can be a Big instance or a BigObject.
+ * @return {boolean} - True if the value is a BigObject, false otherwise.
+ *
+ * @category Utilities
+ *
+ * @example
+ * isBigObject({ value: 123456n, scale: 2 }); // true
+ * isBigObject({ value: 123456n, scale: 2, foo: "bar" }); // true
+ * isBigObject({ value: 123456n }); // false
+ * isBigObject({ scale: 2 }); // false
+ * isBigObject({}); // false
+ * isBigObject(123456n); // false
+ * isBigObject({ value: "abc", scale: "2"}); // false
+ */
+export function isBigObject(value: Big | BigObject | unknown): value is BigObject {
+  // check if value is an object and not null
+  if (value === null || typeof value !== "object")
+    return false;
+
+  // check if value has value and scale properties
+  if ("value" in value && "scale" in value) {
+    // check if value.value is bigint and value.scale is number
+    if (typeof value.value === "bigint" && typeof value.scale === "number")
+      return true;
+  }
+
+  // value is not a BigObject
+  return false;
+}
